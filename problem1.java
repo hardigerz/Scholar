c3bfb5a2f8166cc28ba6f1ed1f99553f2345fdf8
hardigerz <31538507+hardigerz@users.noreply.github.com>
2018-09-11 00:50:03 -0700

    Add files via upload
    answer problem natural number

---
+++ problem1.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String args[]) {
+        
+        int sum=0;
+        for (int i=0;i<500;i++){
+            if(i%4==0){
+                
+                sum+=i;
+                
+            }
+        }
+
+        System.out.println("Sum  = "+sum);
+    }
+}
+Welcome to Alpha Vantage! Here is your API key: R3JHOBZD3A5GS4YK. Please record this API key for lifetime access to Alpha Vantage.
